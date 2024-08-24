98b089ce4ceb7717282d07bf8fb3f3d9a24d7adb
Matt1300 <mateogranizo1304@gmail.com>
2024-08-23 22:43:50 -0500

    Mejorando Identacion

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -9,70 +9,70 @@ import { UserService } from '../../services/user.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-customer',
-  standalone: true,
-  imports: [MaterialModule, RouterLink],
-  templateUrl: './customer.component.html',
-  styleUrl: './customer.component.css'
+    selector: 'app-customer',
+    standalone: true,
+    imports: [MaterialModule, RouterLink],
+    templateUrl: './customer.component.html',
+    styleUrl: './customer.component.css'
 })
 export class CustomerComponent implements OnInit {
 
-  customerList: Customer[] = [];
-  displayedColumns: string[] = ['code', 'name', 'email', 'phone', 'creditlimit', 'status', 'action'];
-  datasource = new MatTableDataSource<Customer>();
-  _permission: MenuPermission = {
-    haveview: false,
-    haveedit: false,
-    havedelete: false,
-    haveadd: false,
-    code: '',
-    name: ''
-  };
+    customerList: Customer[] = [];
+    displayedColumns: string[] = ['code', 'name', 'email', 'phone', 'creditlimit', 'status', 'action'];
+    datasource = new MatTableDataSource<Customer>();
+    _permission: MenuPermission = {
+        haveview: false,
+        haveedit: false,
+        havedelete: false,
+        haveadd: false,
+        code: '',
+        name: ''
+    };
 
-  constructor(
-    private serviceCustomer: CustomerService,
-    private userService: UserService,
-    private toastr: ToastrService
-  ) { }
+    constructor(
+        private serviceCustomer: CustomerService,
+        private userService: UserService,
+        private toastr: ToastrService
+    ) { }
 
 
-  ngOnInit() {
-    this.LoadCustomer();
-    this.SetAccess();
-  }
-
-  SetAccess() {
-    const rol = localStorage.getItem('userRol');
-    if (rol) {
-      this.userService.GetMenuPermission(rol, 'customer').subscribe(data => {
-        this._permission = data;
-      });
-    } else {
-      console.error('No user role found in localStorage');
+    ngOnInit() {
+        this.LoadCustomer();
+        this.SetAccess();
     }
-  }
 
-  LoadCustomer() {
-    this.serviceCustomer.GetAllCustomers().subscribe(data => {
-      this.customerList = data;
-      this.datasource.data = this.customerList;
-    })
-  }
+    SetAccess() {
+        const rol = localStorage.getItem('userRol');
+        if (rol) {
+            this.userService.GetMenuPermission(rol, 'customer').subscribe(data => {
+                this._permission = data;
+            });
+        } else {
+            console.error('No user role found in localStorage');
+        }
+    }
 
-  functionEdit(code: string) {
-    if (this._permission.haveedit) {
-      console.log('Edit ' + code);
-    } else {
-      this.toastr.error('You do not have permission to edit', 'Access Denied');
+    LoadCustomer() {
+        this.serviceCustomer.GetAllCustomers().subscribe(data => {
+            this.customerList = data;
+            this.datasource.data = this.customerList;
+        })
     }
-  }
 
-  functionDelete(code: string) {
-    if (this._permission.havedelete) {
-      console.log('Delete ' + code);
+    functionEdit(code: string) {
+        if (this._permission.haveedit) {
+            console.log('Edit ' + code);
+        } else {
+            this.toastr.error('You do not have permission to edit', 'Access Denied');
+        }
     }
-    else {
-      this.toastr.error('You do not have permission to delete', 'Access Denied');
+
+    functionDelete(code: string) {
+        if (this._permission.havedelete) {
+            console.log('Delete ' + code);
+        }
+        else {
+            this.toastr.error('You do not have permission to delete', 'Access Denied');
+        }
     }
-  }
 }
